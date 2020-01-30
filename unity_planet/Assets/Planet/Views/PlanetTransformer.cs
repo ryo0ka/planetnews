b4c4397775ad1ef1cc036db2583186282c2bd52b
Ryoichiro Oka <ryoka0418@gmail.com>
2020-01-31 04:26:35 +0900

    hand held planet

--- unity_planet/Assets/Planet/Views/PlanetTransformer.cs
+++ unity_planet/Assets/Planet/Views/PlanetTransformer.cs
@@ -1,3 +1,5 @@
+using UniRx;
+using UniRx.Triggers;
 using UnityEngine;
 
 namespace Planet.Views
@@ -8,11 +10,53 @@ namespace Planet.Views
 		Transform _planet;
 
 		[SerializeField]
+		Collider _planetCollider;
+
+		[SerializeField]
+		Collider _leftControllerCollider;
+
+		[SerializeField]
 		float _spinSpeed;
 
-		void Update()
+		bool _canHold;
+
+		Vector3 _leftControllerPositionOffset;
+		Quaternion _leftControllerRotationOffset;
+
+		void Start()
 		{
-			_planet.Rotate(Vector3.up, Time.deltaTime * _spinSpeed, Space.Self);
+			_planetCollider
+				.OnTriggerEnterAsObservable()
+				.Where(c => c == _leftControllerCollider)
+				.Subscribe(_ => _canHold = true)
+				.AddTo(this);
+
+			_planetCollider
+				.OnTriggerExitAsObservable()
+				.Where(c => c == _leftControllerCollider)
+				.Subscribe(_ => _canHold = false)
+				.AddTo(this);
+		}
+
+		void LateUpdate()
+		{
+			var leftController = _leftControllerCollider.transform;
+
+			if (OVRInput.GetDown(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+			{
+				_leftControllerPositionOffset = leftController.InverseTransformVector(_planet.position - leftController.position);
+				_leftControllerRotationOffset = Quaternion.Inverse(leftController.rotation) * _planet.rotation;
+			}
+
+			if (_canHold && OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger, OVRInput.Controller.LTouch))
+			{
+				_planet.position = leftController.position + leftController.TransformVector(_leftControllerPositionOffset);
+				_planet.rotation = leftController.rotation * _leftControllerRotationOffset;
+			}
+			else
+			{
+				_planet.Rotate(Vector3.up, Time.deltaTime * _spinSpeed, Space.Self);
+			}
 		}
 	}
 }
