1791a80154ae7c6ca7dff0140962a4c7beaa0a8c
Ryoichiro Oka <ryoka0418@gmail.com>
2020-02-01 20:47:05 +0900

    fix panel orientation

--- unity_planet/Assets/Planet/Views/PlanetCanvasTransformer.cs
+++ unity_planet/Assets/Planet/Views/PlanetCanvasTransformer.cs
@@ -1,3 +1,4 @@
+using Planet.Utils;
 using UnityEngine;
 
 namespace Planet.Views
@@ -15,13 +16,13 @@ namespace Planet.Views
 		Canvas _planetCanvas;
 
 		[SerializeField]
-		RectTransform _leftPanel;
+		RectTransform _leftPanelCenter;
 
 		[SerializeField]
 		RectTransform _leftPanelRoot;
 
 		[SerializeField]
-		RectTransform _rightPanel;
+		RectTransform _rightPanelCenter;
 
 		[SerializeField]
 		RectTransform _rightPanelRoot;
@@ -38,33 +39,38 @@ namespace Planet.Views
 		{
 			_canvasRoot.position = _planetScaler.position;
 
-			UpdatePanelTransform(true, _leftPanelRoot, _leftPanel);
-			UpdatePanelTransform(false, _rightPanelRoot, _rightPanel);
+			UpdatePanelTransform(true, _leftPanelRoot, _leftPanelCenter);
+			UpdatePanelTransform(false, _rightPanelRoot, _rightPanelCenter);
 		}
 
-		void UpdatePanelTransform(bool left, RectTransform panelRoot, RectTransform panel)
+		void UpdatePanelTransform(bool left, RectTransform panelRoot, RectTransform panelCenter)
 		{
 			// Calculate margin between the panel & planet
 			var planetScale = _planetScaler.lossyScale.x;
 			var canvasScale = _planetCanvas.transform.lossyScale.x;
-			var marginsSize = planetScale / canvasScale / 2;
+			var marginSize = planetScale / canvasScale / 2;
 
 			// Calculate the angle that keeps the panel against the camera
-			var h = marginsSize + panel.sizeDelta.x / 2;
-			var p = new Vector2(_mainCameraTransform.position.x, _mainCameraTransform.position.z);
-			var o = new Vector2(_planetScaler.position.x, _planetScaler.position.z);
-			var op = (p - o).magnitude / canvasScale;
+			var p = _mainCameraTransform.position;
+			var o = _planetScaler.position;
+			var op = (p - o).Multiply(1, 0, 1).magnitude / canvasScale;
+			var h = (panelCenter.position - o).Multiply(1, 0, 1).magnitude / canvasScale;
 			var np = Mathf.Sqrt(op * op - h * h);
 			var od = Mathf.Atan(np / h) * Mathf.Rad2Deg;
+			var odlr = left ? od - 90 : 90 - od;
+			odlr = float.IsNaN(odlr) ? 0 : odlr;
+
+			//Debug.Log($"{marginSize: 0.00} {panel.sizeDelta.x: 0.00}");
+			Debug.DrawLine(o, panelCenter.position, Color.white);
 
 			// Apply rotation
 			panelRoot.LookAt(_mainCameraTransform.position, Vector3.up);
 			panelRoot.Rotate(0, 180, 0, Space.Self);
-			panelRoot.Rotate(0, left ? od - 90 : 90 - od, 0, Space.Self);
+			panelRoot.Rotate(0, odlr, 0, Space.World);
 
 			// Apply margin
 			var sizeDelta = panelRoot.sizeDelta;
-			sizeDelta.x = marginsSize;
+			sizeDelta.x = marginSize;
 			panelRoot.sizeDelta = sizeDelta;
 		}
 	}
